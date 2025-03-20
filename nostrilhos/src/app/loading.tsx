e2bf7b7b8f8752b3b8ca7b6c27094a87ee177e3f
juliabusso <juliabusso@hotmail.com>
2025-03-19 21:25:29 -0300

    Página Loading feita

---
+++ nostrilhos/src/app/loading.tsx
@@ -0,0 +1,8 @@
+export default function loading() {
+
+    return (
+        <div className="h-screen w-full flex items-center justify-center">
+            <p className="text-4xl text-orange-500"> Carregando...</p>
+        </div>
+    )
+}
